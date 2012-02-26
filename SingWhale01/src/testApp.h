fd358362d25a09474a89bb1e68365d68677bccd1
liamartinez <sushionthego@gmail.com>
2012-02-26 00:45:22 -0500

    first commit

---
+++ SingWhale01/src/testApp.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxiPhone.h"
+#include "ofxiPhoneExtras.h"
+
+#include "ofxTweenzor.h"
+
+#include "swSceneManager.h"
+
+//create the main scenes
+
+//bottom menu
+
+class testApp : public ofxiPhoneApp {
+	
+public:
+	void setup();
+	void update();
+	void draw();
+	void exit();
+	
+	void touchDown(ofTouchEventArgs &touch);
+	void touchMoved(ofTouchEventArgs &touch);
+	void touchUp(ofTouchEventArgs &touch);
+	void touchDoubleTap(ofTouchEventArgs &touch);
+	void touchCancelled(ofTouchEventArgs &touch);
+
+	void lostFocus();
+	void gotFocus();
+	void gotMemoryWarning();
+	void deviceOrientationChanged(int newOrientation);
+
+};
+
+
